eb2898094794b68a041d9686c24ea704222408a1
Cavalcanti <carlos.d.cavalcanti@accenture.com>
2017-08-17 11:04:17 -0300

    ToDo List

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+
+  tasks: string[][] = [
+    ['Comprar pão', 'Ir até a padaria e comprar 0,5kg de Pão'],
+    ['Lavar os pratos', 'Lavar toda a loça da cozinha'],
+    ['Trabalhar', 'Terminar projeto em Angular 4'],
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  addTask(titulo: string, descricao: string) {
+    this.tasks.push([titulo, descricao]);
+  }
+
+  remover(index: number) {
+    this.tasks.splice(index);
+  }
+
+}
